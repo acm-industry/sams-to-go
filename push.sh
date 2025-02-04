19256e2892cd0589386a88303adadca4675cdfd4
Aman Desai <amandesai@ucsb.edu>
2025-02-03 21:55:17 -0800

    Ugh Bugs

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-unzip -o ~/Downloads/sams-to-go.webflow.zip -d . && rm ~/Downloads/sams-to-go.webflow.webflow.zip
+unzip -o ~/Downloads/sams-to-go.webflow.zip -d . && rm ~/Downloads/sams-to-go.webflow.zip
 git add .
 git commit -m "$1"
 git push
